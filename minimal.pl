e9015ef21b100f5ee57fc2f930ac486408c3ee55
kferles <kferles@gmail.com>
2013-02-25 17:28:03 +0200

    perl version (to gbalats with <3)

---
+++ minimal.pl
@@ -0,0 +1,12 @@
+
+while(<STDIN>){
+    @tmp = split('\||\n');
+    push @pairs, [@tmp];
+    $values{$tmp[1]}{$tmp[0]} = $tmp[0];
+}
+
+foreach(@pairs){
+    if(keys(%{$values{@$_[1]}}) >= $ARGV[0]){
+	print "@$_[0]|@$_[1]\n";
+    }
+}
