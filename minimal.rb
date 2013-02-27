71a57a76b109cbc54a5627f0cd0bb52b94fdffe1
kferles@gmail.com <kostas@kostas-CQ2702EVm.(none)>
2013-02-28 00:20:40 +0200

    gbalat's f*cking issue, fixed

--- minimal.rb
+++ minimal.rb
@@ -2,6 +2,6 @@ k=ARGV[0].to_i
 require 'set'
 pairs = $stdin.map {|line| x,y = line.chop.split('|')}
 vals = {}
-pairs.each{ |x,y| vals[y] = (vals[y] || Set.new).add(x) }
-pairs.each{ |x,y| if(vals[y].size() >= k) then printf "%d|%d\n",x,y end }
+pairs.each{ |x,y| vals[y.to_i] = (vals[y.to_i] || Set.new).add(x.to_i) }
+pairs.each{ |x,y| if(vals[y.to_i].size() >= k) then printf "%d|%d\n",x,y end }
 
