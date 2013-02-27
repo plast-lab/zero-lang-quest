192ace90ec246acba89c62f7877409e8edde87ae
kferles <kferles@gmail.com>
2013-02-28 00:21:54 +0200

    gbalat's f*cking issue, fixed

--- minimal.rb
+++ minimal.rb
@@ -4,4 +4,3 @@ pairs = $stdin.map {|line| x,y = line.chop.split('|')}
 vals = {}
 pairs.each{ |x,y| vals[y.to_i] = (vals[y.to_i] || Set.new).add(x.to_i) }
 pairs.each{ |x,y| if(vals[y.to_i].size() >= k) then printf "%d|%d\n",x,y end }
-
