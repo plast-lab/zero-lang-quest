923bd5e983bea084f39154dc73a5bef7d27fd4cb
George Balatsouras <gbalats@gmail.com>
2013-11-25 16:46:36 +0200

    making haskell more readable and removing dependency

--- minimal.hs
+++ minimal.hs
@@ -1,15 +1,26 @@
-import Data.Map (Map, fromListWith, (!))
-import Data.Set (Set, union, singleton, size)
-import Data.List.Split
+import Data.Map (fromListWith, (!))
+import Data.Set (union, singleton, size)
+import Data.Text (pack, unpack, split)
 import System.Environment
 
-tuplify2 [x,y] = (x,y)
+type Pair = (Int, Int)
 
-pipeline k (x:xs) = let parseLine = tuplify2 . map (\x -> read x::Integer) . splitOn "|"
-                        trans = (\(x,y) -> (y, singleton x)) . parseLine
-                        m = (fromListWith union . map trans) (x:xs)
-                    in filter (\z -> (size (m ! (snd . parseLine) z) >= k)) (x:xs)
+parseInt x = read x::Int
 
+parse :: String -> Pair
+parse = tuplify . map (parseInt . unpack) . split (=='|') . pack
+        where tuplify [x,y] = (x,y)
+
+unparse :: Pair -> String
+unparse (x,y) = show x ++ "|" ++ show y
+
+multipair :: Int -> [String] -> [String]
+multipair k lines = let pairs    = map parse lines
+                        tr (x,y) = (y, singleton x)
+                        invmap   = fromListWith union $ map tr pairs
+                        oc (_,y) = (>=k) $ size $ invmap ! y
+                     in map unparse $ filter oc pairs
 main = do
   args <- getArgs
-  interact $ unlines. pipeline (read (head args)::Int). lines
+  let n = parseInt $ head args 
+   in interact $ unlines . multipair n . lines
