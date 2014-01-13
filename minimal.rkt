dd7c49a557aff5d14cc790ba8f36aead44d6c60a
Aggelos Biboudis <biboudis@gmail.com>
2014-01-13 17:54:31 +0200

    Reading from standard input.

--- minimal.rkt
+++ minimal.rkt
@@ -1,7 +1,9 @@
 #lang racket
-
-(let ([lst (map (λ (x) (string-split x "|")) (file->lines "test.in"))] 
+(require racket/sequence)
+(let ([lst (sequence->list (sequence-map (λ (x) (string-split x "|")) (in-lines)))] 
       [h (make-hash)])
-  (map (λ (x) (if (hash-has-key? h (cadr x) ) (hash-update! h (cadr x) (λ (y) (set-add y (car x))))
+  (map (λ (x) (if (hash-has-key? h (cadr x) ) 
+                  (hash-update! h (cadr x) (λ (y) (set-add y (car x))))
                   (hash-set! h (cadr x) (set (car x))))) lst)
-  (for ([i lst] #:when (> (set-count (hash-ref h (cadr i))) 2)) (printf "~a\n" i)))
+  (for ([i lst] #:when (> (set-count (hash-ref h (cadr i))) 1)) 
+    (printf "~a|~a\n" (car i)(cadr i))))
